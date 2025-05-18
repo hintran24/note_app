8a4e36966f4f7f64664c79dd0c10b8655b3f59e7
hintran24 <106430650+hintran24@users.noreply.github.com>
2025-05-18 21:08:13 +0700

    Delete banglienquan.py

--- banglienquan.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('notes.db')
-cursor = conn.cursor()
-cursor.execute('SELECT * FROM note_categories')
-print("Note-Categories:", cursor.fetchall())
-cursor.execute('SELECT * FROM note_tags')
-print("Note-Tags:", cursor.fetchall())
